46564c00492d014062369edaec42d58aa0246fd7
nicajonh <nicajonh@126.com>
2020-04-21 22:06:56 +0800

    修复dcl现场安全问题

--- src/main/java/com/tida/manual/SingTone.java
+++ src/main/java/com/tida/manual/SingTone.java
@@ -7,14 +7,14 @@ package com.tida.manual;/**
  * @ClassName SingTone
  * 以时间换空间，有一定几率存在线程安全问题
  * @Description 双重锁懒汉模式,这种看起来很完美的优化技巧就是double-checked locking，根据JLS规范，上面的代码不可靠！线程有可能得到一个不为null，但是构造不完全的对象,
- * 原因是编译器为了提高执行效率的指令重排。只要认为在单线程下是没问题的，它就可以进行乱序写入！以保证不要让cpu指令流水线中断
+ * 原因是编译器为了提高执行效率的!!指令重排!!。只要认为在单线程下是没问题的，它就可以进行乱序写入！以保证不要让cpu指令流水线中断
  * @Author Administrator
  * @Date 2020/4/21 21:06
  * @Version 1.0
  **/
 class DlcSingTone {
     //dcl双重懒汉模式
-    private static DlcSingTone INSTANCE=null;
+    private static DlcSingTone INSTANCE=null;  //使用volatile可以禁止指令的重排序,heppened before原则
     private DlcSingTone(){}
     public static DlcSingTone getDlcSingTone() {
         if (null == INSTANCE) {
